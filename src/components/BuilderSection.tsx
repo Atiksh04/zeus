89de0bc6a364b1146b53c2fd37881ea10b9f6df2
Atiksh Srivastava <ayushs125@gmail.com>
2023-08-04 20:11:00 +0530

    add: sections

---
+++ src/components/BuilderSection.tsx
@@ -0,0 +1,11 @@
+
+
+const BuilderSection = ()=>{
+    return(
+        <div className="h-full w-full text-black">
+            Builder Section
+        </div>
+    )
+}
+
+export default BuilderSection;
---
+++ src/components/PanelSection.tsx
@@ -0,0 +1,11 @@
+
+const PanelSection = () => {
+    return (
+        <div className="h-full w-full text-black border border-left border-slate-300">
+            Panel Section
+        </div>
+    )
+};
+
+
+export default PanelSection;
